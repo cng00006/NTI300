f2979cdfdf1b751a859cd6091b93184753f3bda4
cng00006 <55862984+cng00006@users.noreply.github.com>
2019-10-23 21:16:17 -0700

    Update cut.sh

--- cut.sh
+++ cut.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 who | cut -d' ' -f1
+#This allows you to cut out the list of users by their name. It helps allow automation in the example that followed by sending the user
+#name to mail.
