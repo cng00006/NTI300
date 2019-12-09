745d8878b000deae33dc071202616c2b3685cbcb
cng00006 <55862984+cng00006@users.noreply.github.com>
2019-12-09 15:25:38 -0800

    Update django.py

--- django.py
+++ django.py
@@ -68,37 +68,6 @@ def django_start():
         f.close()
     os.system('sudo -u django sh -c "source /opt/django/django-env/bin/activate && python manage.py runserver 0.0.0.0:8000&"')
 
-def setup_mod_wsgi():
-    print('********** setup mod wsgi install')
-
-    os.chdir('/opt/django/project1')
-
-    # update settings.py
-    new_string = 'STATIC_ROOT = os.path.join(BASE_DIR, "static/")' + '\n'
-    print (new_string)
-
-    with open('project1/settings.py', "a") as f:
-        f.write(new_string)
-    with open('project1/settings.py') as f:
-        f.close()
-    print('********** settings.py updated')
-    # update the django.conf file for httpd
-    # define django.conf file content as an array
-    django_config_file = [
-        'Alias /static /opt/django/project1/static/',
-        '<Directory /opt/django/project1/static/>',
-        '    Require all granted',
-        '</Directory>',
-        '<Directory /opt/django/project1/project1>',
-        '    <Files wsgi.py>',
-        '        Require all granted',
-        '    </Files>',
-        '</Directory>',
-        'WSGIDaemonProcess project1 python-path=/opt/django/project1:/opt/django/django-env/lib/python2.7/site-packages/',
-        'WSGIProcessGroup project1',
-        'WSGIScriptAlias / /opt/django/project1/project1/wsgi.py'
-        ]
-
     f = open('/etc/httpd/conf.d/django.conf',"w+")
     i = 0
     while i < len(django_config_file):
